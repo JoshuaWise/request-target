af957c0ed82bb5f6900cd43b0911d84430d39bdb
Joshua Wise <joshuathomaswise@gmail.com>
2018-01-30 02:01:33 -0500

    updated parser to be more similar to standards

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 'use strict';
 const { TLSSocket } = require('tls');
 const punycode = require('punycode');
-const query = require('./lib/query');
-const authority = require('./lib/authority');
-const absolutePath = require('./lib/absolute-path');
+const requestTarget = require('./lib/request-target');
+const hostHeader = require('./lib/host-header');
 
-// https://tools.ietf.org/html/rfc7230#section-5.3
-const requestTarget = new RegExp(`^(?:(https?)://${authority}(?!\\*)|(?=[/*]))(?:(${absolutePath}|\\*$)${query}?)?$`, 'i');
-
-// https://tools.ietf.org/html/rfc7230#section-5.4
-const hostHeader = new RegExp(`^(?:${authority})?\\s*$`, 'i');
-
+// https://tools.ietf.org/html/rfc7230#section-5.5
 module.exports = (req) => {
 	const target = req.url.match(requestTarget);
 	if (!target) return null; // Invalid url
 	
-	let [,scheme = '', host = '', port = '', path = '', query = ''] = target;
+	let [,scheme = null, host = null, port = null, path, query = ''] = target;
 	const isSecure = req.socket instanceof TLSSocket;
 	
 	if (scheme) {
@@ -25,21 +19,27 @@ module.exports = (req) => {
 		const hostport = req.headers.host.match(hostHeader);
 		if (!hostport) return null; // Invalid Host header
 		if (hostport[1]) {
-			[,host, port = ''] = hostport;
-			scheme = isSecure ? 'https' : 'http';
+			[,host, port = null] = hostport;
+			scheme = isSecure ? 'https:' : 'http:';
 		}
 	}
 	
-	if (port && +port > 65535) return null; // Invalid port number
 	if (host) {
 		// DNS names have a maximum length of 255, including the root domain.
 		if (host.length > (host.charCodeAt(host.length - 1) === 46 ? 255 : 254)) return null;
-		if (!port) port = isSecure ? '443' : '80';
 		host = host.toLowerCase();
 		// Fake punycode labels are not valid IDNA labels.
 		// https://tools.ietf.org/html/rfc5890#section-2.3.1
 		if (host.startsWith('xn--') && !punycode.toUnicode(host)) return null;
+		if (port) {
+			const num = +port;
+			if (num > 65535) return null; // Invalid port number
+			if (port.charCodeAt(0) === 48) port = '' + num; // Remove leading zeros
+		} else {
+			port = isSecure ? '443' : '80';
+		}
 	}
 	
-	return { scheme, host, port, path, query };
+	// Rename fields to make it familiar with standard APIs.
+	return { protocol: scheme, hostname: host, port, pathname: path, search: query };
 };
